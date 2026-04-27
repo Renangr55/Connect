c9f98ae79c6c90aadded04d551fc5217ee8c18f5
Renangr55 <renan.g.rodrigues5@gmail.com>
2026-04-26 21:25:15 -0300

    Chat conectado

--- back/project/services/wattsonIA.py
+++ back/project/services/wattsonIA.py
@@ -1,40 +1,93 @@
 from ibm_watsonx_ai.foundation_models import ModelInference
+import requests
 
-def get_model():
-    return ModelInference(
-        model_id="ibm/granite-8b-code-instruct",
-        credentials={
-            "apikey": "pl-GAQdbzYIG7Fuf1agZHNHgUz0DWhFUMtdwF_KH_aXy",
-            "url": "https://us-south.ml.cloud.ibm.com"
-        },
-        project_id="b6b7400c-2822-45c3-991a-15a5f77eef71"
-    )
+
+# def get_model():
+#     return ModelInference(
+#         model_id="meta/llama-3-3-70b-instruct",
+#         credentials={
+#             "apikey": "04Icbd8uoPKwA8kKJO88nMkoKlM6XVhSrKBxkxFcfpq2",
+#             "url": "https://us-south.ml.cloud.ibm.com"
+#         },
+#         project_id="59ffbf5a-c4ab-45a4-8e6f-ff57ee18f40a"
+#     )
     
+
+API_KEY = "04Icbd8uoPKwA8kKJO88nMkoKlM6XVhSrKBxkxFcfpq2"
+PROJECT_ID = "59ffbf5a-c4ab-45a4-8e6f-ff57ee18f40a"
+
+def get_token():
+    url = "https://iam.cloud.ibm.com/identity/token"
+
+    data = {
+        "grant_type": "urn:ibm:params:oauth:grant-type:apikey",
+        "apikey": API_KEY
+    }
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.post(url, data=data, headers=headers)
+    return response.json()["access_token"]
 
 
 def send_message(text):
-    prompt = f"""
-    Responda APENAS com uma frase clara.
+    token = get_token()
 
-    Pergunta: {text}
+    url = "https://eu-gb.ml.cloud.ibm.com/ml/v1/text/chat?version=2023-05-29"
 
-    Se souber a resposta, responda diretamente.
-    Se não souber, diga: "Não sei".
+    body = {
+        "messages": [
+            {
+                "role": "user",
+                "content": text
+            }
+        ],
+        "project_id": PROJECT_ID,
+        "model_id": "meta-llama/llama-3-3-70b-instruct",
+        "temperature": 0.7,
+        "max_tokens": 500
+    }
 
-    Resposta:
-    """
-    model = get_model()
-    response = model.generate(prompt=prompt)
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
 
-    try:
-        result = response["results"][0]["generated_text"].strip()
+    response = requests.post(url, headers=headers, json=body)
 
-        # 🔥 tratamento inteligente
-        if not result or result in ["?", ".", ""]:
-            return "Não consegui gerar uma resposta agora."
+    if response.status_code != 200:
+        return response.text
 
-        return result
+    data = response.json()
 
-    except Exception as e:
-        return "Erro ao gerar resposta."
+    return data["choices"][0]["message"]["content"]
+
+# def send_message(text):
+#     prompt = f"""
+#     Responda APENAS com uma frase clara.
+
+#     Pergunta: {text}
+
+#     Se souber a resposta, responda diretamente.
+#     Se não souber, diga: "Não sei".
+
+#     Resposta:
+#     """
+#     model = get_model()
+#     response = model.generate(prompt=prompt)
+
+#     try:
+#         result = response["results"][0]["generated_text"].strip()
+
+        
+#         if not result or result in ["?", ".", ""]:
+#             return "Não consegui gerar uma resposta agora."
+
+#         return result
+
+#     except Exception as e:
+#         return "Erro ao gerar resposta."
 
